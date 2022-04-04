1f5ac552a02c4bcf22f1b4188508a148b39ed6ca
Cory Parsnipson <cparsnipson@gmail.com>
2022-04-03 18:31:31 -0700

    Make categories wrap. Add *.draft.md to ignore.

--- src/lib/components/Categories.svelte
+++ src/lib/components/Categories.svelte
@@ -2,7 +2,7 @@
   export let categories = [];
 </script>
 
-<div class="flex gap-2">
+<div class="flex flex-wrap gap-2">
   {#each categories as category}
     <a class="py-1 px-4 rounded-2xl hover:bg-slate-400 active:bg-slate-50 bg-slate-300 text-base">
       {category}
