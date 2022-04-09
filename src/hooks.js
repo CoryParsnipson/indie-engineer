916ee0330407cb85e6e00ca6860be12404d092c4
Cory Parsnipson <cparsnipson@gmail.com>
2022-04-09 15:08:35 -0700

    Change code to permanent redirect.

--- src/hooks.js
+++ src/hooks.js
@@ -6,7 +6,7 @@ export async function handle({ event, resolve }) {
   if (event.url.host !== 'sveltekit-prerender' && (proto_header && proto_header !== 'https')) {
     let https_url = event.url.href.replace(/^(http)/, 'https');
     console.log(https_url);
-    return Response.redirect(https_url, 301);
+    return Response.redirect(https_url, 308);
   }
 
   // else don't do anything to the request
