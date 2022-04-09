b799194b9d08b8d33f847d4cb381f11b1759bcf0
Cory Parsnipson <cparsnipson@gmail.com>
2022-04-09 11:58:38 -0700

    Implement http to https redirect.
    See here:
    https://help.heroku.com/J2R1S4T8/can-heroku-force-an-application-to-use-ssl-tls
    
    here:
    https://kit.svelte.dev/docs/hooks#handle
    
    and here:
    https://stackoverflow.com/questions/71360354/redirecting-from-exported-handle-hook-in-sveltekit
    
    The object is that the Heroku router will set X-Forwarded-Proto in the
    request header and leaves it up to the application to check if this
    value is not 'https' (is 'http' or something) and then redirect.
    
    The second link shows how to use hooks that run on every request to
    do the check and then the third link is an example of how to use
    SvelteKit hooks to do a redirect.

---
+++ src/hooks.js
@@ -0,0 +1,14 @@
+/** @type {import('@sveltejs/kit').Handle} */
+export async function handle({ event, resolve }) {
+  let proto_header = event.request.headers.get('X-Forwarded-Proto');
+
+  // redirect non https to https
+  if ((proto_header && proto_header !== 'https') || !event.url.href.startsWith('https')) {
+    let https_url = event.url.href.replace(/^(http)/, 'https');
+    return Response.redirect(https_url, 303);
+  }
+
+  // else don't do anything to the request
+  const response = await resolve(event);
+  return response;
+}
