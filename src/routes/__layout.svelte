1969749eaa7e0aca612e18f9c1b7a5955bfff4be
Cory Parsnipson <cparsnipson@gmail.com>
2022-04-18 22:19:28 -0700

    Add footer formatting and some content.

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,6 +3,8 @@
 
 	import '../app.css';
   import { env } from '$lib/util/env';
+
+  let current_year = new Date().getFullYear();
 </script>
 
 <GoogleTagManager
@@ -13,8 +15,8 @@
 
 <header class="sticky top-0 w-full z-50 bg-cyan-800 text-slate-100">
   <div class="flex items-stretch gap-4 max-w-screen-lg sm:mx-auto pl-5 sm:px-5 bg-green-100">
-    <img src="/indie-engineer-logo.png" class="p-2 pl-0 hidden sm:block">
-    <img src="/indie-engineer-logo-sm.png" class="py-1 pr-2 pl-0 sm:hidden">
+    <img alt="Indie Engineer logo" src="/indie-engineer-logo.png" class="p-2 pl-0 hidden sm:block">
+    <img alt="Indie Engineer logo" src="/indie-engineer-logo-sm.png" class="py-1 pr-2 pl-0 sm:hidden">
 
     <div class="nav dropdown grow bg-zinc-800">
       <div class="justify-end flex items-center h-full mx-5 md:hidden">
@@ -35,10 +37,30 @@
   <slot />
 </div>
 
-<footer class="p-3 z-50 bg-cyan-800 text-slate-100">
+<footer class="z-50 bg-cyan-800 text-slate-100">
+  <div class="flex flex-col-reverse md:flex-row items-stretch justify-center max-w-screen-lg mx-auto">
+    <div class="md:w-1/2 lg:w-3/5 p-3 flex flex-col justify-end">
+      <p class="grow"></p>
+      <p class="font-title mb-3">This website was made with SvelteKit, TailwindCSS, and MDsveX. <a class="font-title" href="https://www.flaticon.com/free-icons/email" title="email icons">Email icons created by Chanut - Flaticon</a>.</p>
+      <p class="font-title">Copyright © {current_year} Cory Parsnipson. All Rights Reserved.</p>
+    </div>
+
+    <div class="md:w-1/2 lg:w-2/5 bg-stone-300">
+      <img src="/indie-engineer-logo.png" alt="Indie Engineer logo" width="250px" class="mx-auto pt-8 pb-4" />
+      <p class="font-title text-slate-800 text-center mb-6">Contact</p>
+      <div class="flex gap-5 justify-center mb-12">
+        <img src="/email.png" width="50px" alt="Click here to email the authors">
+        <a href="https://github.com/CoryParsnipson" target="_blank" rel="nofollow noopener">
+          <img src="/GitHub-Mark-120px-plus.png" width="50px" alt="Click here to go to Cory Parsnipson's GitHub page.">
+        </a>
+        <a href="https://twitter.com/CoryParsnipson" target="_blank" rel="nofollow noopener">
+          <img src="/twitter-icon.png" width="50px" alt="Click here to visit Cory Parsnipson's twitter.">
+        </a>
+      </div>
+    </div>
+  </div>
+
   <div class="font-title text-xl max-w-screen-lg sm:mx-auto">
-    This website was made with sveltekit + tailwindcss + mdsvex.
-    (And svelte, postcss, ...)
   </div>
 </footer>
 
