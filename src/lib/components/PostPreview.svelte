6ee11dd8c83f984ca42f6f5170a98b2725a4edd5
Cory Parsnipson <cparsnipson@gmail.com>
2022-04-22 19:45:04 -0700

    Remove header tags from PostPreview component.
    Caught by lighthouse score audit as SEO problems. Headers were presented
    out of order.

--- src/lib/components/PostPreview.svelte
+++ src/lib/components/PostPreview.svelte
@@ -9,12 +9,12 @@
 
 <div class="bg-slate-100 rounded-2xl p-4 mb-5">
   <a href="{path}">
-    {#if metadata.date}<h6>{date}</h6>{/if}
+    {#if metadata.date}<p>{date}</p>{/if}
     <h2 class="text-3xl font-bold tracking-tighter mb-4">{metadata.title}</h2>
     {#if metadata?.categories?.length > 0}
       <Categories categories={metadata.categories} />
     {/if}
-    <h5 class="text-slate-500 mt-4">{metadata.readingTime.text}</h5>
+    <p class="text-slate-500 mt-4">{metadata.readingTime.text}</p>
     {#if metadata.summary}<p class="mt-6 mb-0">{metadata.summary}</p>{/if}
   </a>
 </div>
