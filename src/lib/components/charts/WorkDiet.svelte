f62b249adc3c69b29f89ed5c832f1b7dfd233abf
Cory Parsnipson <cparsnipson@gmail.com>
2022-04-24 12:34:40 -0700

    Increase contrast in work diet chart
    Following lighthouse score suggestions.

--- src/lib/components/charts/WorkDiet.svelte
+++ src/lib/components/charts/WorkDiet.svelte
@@ -66,7 +66,7 @@
       <div class="flex flex-wrap items-start justify-center gap-2 mx-auto mx-6 sm:mx-12 py-5">
       {#each row.items as item}
         <p class="m-0 py-2 px-4 z-10
-          text-slate-100 bg-slate-500 rounded-lg
+          text-zinc-50 bg-zinc-700 rounded-lg
           font-sans leading-normal text-center
           text-xs min-w-[2rem] max-w-[7rem]
           sm:text-lg sm:min-w-[5rem] sm:max-w-[10rem]">
