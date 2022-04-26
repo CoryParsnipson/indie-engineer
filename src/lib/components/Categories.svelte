09d43f739dfbb431e40bdf926cf80279b8988150
Cory Parsnipson <cparsnipson@gmail.com>
2022-04-25 17:21:54 -0700

    Change categories font to title font

--- src/lib/components/Categories.svelte
+++ src/lib/components/Categories.svelte
@@ -4,7 +4,7 @@
 
 <div class="flex flex-wrap gap-2">
   {#each categories as category}
-    <a class="py-1 px-4 rounded-lg text-lg font-serif text-cream-800 bg-cream-300
+    <a class="py-1 px-4 rounded-lg text-base font-title text-cream-800 bg-cream-300
               hover:bg-cream-400 hover:cursor-pointer active:bg-cream-200">
       {category}
     </a>
