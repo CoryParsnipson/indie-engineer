c6ee5349a142bd9fad0b4e98f8fe044dc20adddc
Cory Parsnipson <cparsnipson@gmail.com>
2022-04-30 00:27:54 -0700

    Change style on tag page header 1

--- src/routes/tag.svelte
+++ src/routes/tag.svelte
@@ -39,7 +39,7 @@
 </svelte:head>
 
 <main class="py-12">
-  <h1 class="font-title leading-relaxed text-5xl mb-12">Posts Tagged with "{tag}":</h1>
+  <h1 class="font-title leading-relaxed text-4.5xl mb-12">Posts Tagged with "<span class="font-title leading-relaxed text-4.5xl text-emerald-800">{tag}</span>":</h1>
 
   <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
     {#each posts as post}
